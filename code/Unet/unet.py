ae069bc9bb3ffd85e5411819ede5003c412a40ad
harsha-ys <hyoshith@gmail.com>
2023-01-10 04:45:52 +0530

    contradiction path(Encoder path) for an unet

---
+++ code/Unet/unet.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 30 20:16:54 2022
+
+@author: HARSHANA
+"""
+
+import tensorflow as tf
+IMG_width = 128
+IMG_height = 128
+IMG_chanel = 3
+
+#build the chanel
+
+#input layer
+inputs = tf.keras.layers.Input((IMG_width, IMG_height, IMG_chanel))
+
+s= tf.keras.layers.Lambda(lambda x:x/255)(inputs)#convert integer rgb values into floats within the range 0-1
+
+
+#contradiction path(Encoder path)
+c1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+c1 = tf.keras.layers.Dropout(0.1)(c1)#drop out to generalize
+c1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+p1 = tf.keras.layers.MaxPool2D((2,2))(c1)
+
+c2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+c2 = tf.keras.layers.Dropout(0.1)(c2)#drop out to generalize
+c2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+p2 = tf.keras.layers.MaxPool2D((2,2))(c2)
+
+c3 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+c3 = tf.keras.layers.Dropout(0.2)(c3)#drop out to generalize
+c3 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+p3 = tf.keras.layers.MaxPool2D((2,2))(c3)
+
+c4 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+c4 = tf.keras.layers.Dropout(0.2)(c4)#drop out to generalize
+c4 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+p4 = tf.keras.layers.MaxPool2D((2,2))(c4)
+
+c5 = tf.keras.layers.Conv2D(256, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+c5 = tf.keras.layers.Dropout(0.2)(c5)#drop out to generalize
+c5 = tf.keras.layers.Conv2D(256, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
